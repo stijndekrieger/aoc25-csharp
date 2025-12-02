51c2b0fb8b808d2ca7d8b1d76f5dbf9e7239c470
Stijn de Krieger <sdekrieger@auxilium.nl>
2025-12-02 16:50:08 +0100

    Day 2 part 2

--- AdventOfCode2025/Day02/Day02.cs
+++ AdventOfCode2025/Day02/Day02.cs
@@ -21,45 +21,75 @@ public class Day02
         {
             for (var id = Start; id <= End; id++)
             {
-                if (!IsValidIdPart1(id))
-                {
+                if (IsInvalidIdPart1(id))
                     invalidIdSumPart1 += id;
-                }
 
-                if (!IsValidIdPart2(id))
-                {
+                if (IsInvalidIdPart2(id))
                     invalidIdSumPart2 += id;
-                }
             }
         }
 
         Console.WriteLine("Day 2 Part 1: " + invalidIdSumPart1); //56660955519
-        Console.WriteLine("Day 2 Part 2: " + invalidIdSumPart2);
+        Console.WriteLine("Day 2 Part 2: " + invalidIdSumPart2); //79183223243
     }
 
-    private static bool IsValidIdPart1(ulong id)
+    private static bool IsInvalidIdPart1(ulong id)
     {
         var idString = id.ToString();
 
         // Any uneven digit length is valid, since no sequence can be repeated
         if (idString.Length % 2 != 0)
-            return true;
+            return false;
 
         var firstHalf = idString.Substring(0, idString.Length / 2);
         var secondHalf = idString.Substring(idString.Length / 2);
 
         if (firstHalf == secondHalf)
-            return false;
+            return true;
 
-        return true;
+        return false;
     }
 
-    private static bool IsValidIdPart2(ulong id)
+    private static bool IsInvalidIdPart2(ulong id)
     {
         var idString = id.ToString();
 
-        // TODO
+        // Single digits are always valid
+        if (idString.Length == 1)
+            return false;
 
-        return true;
+        // If all digits are the same, ID is invalid
+        if (idString.Distinct().Count() == 1)
+            return true;
+
+        var idLength = idString.Length;
+        var maxDenominatorToCheck = idLength / 2;
+
+        for (var denominator = 2; denominator <= maxDenominatorToCheck; denominator++)
+        {
+            if (idLength % denominator != 0) continue;
+
+            var firstSubstring = idString.Substring(0, denominator);
+            var maxDenominatorSubstringsInId = idLength / denominator;
+            var matches = 0;
+
+            for (var substringStartAt = 0; substringStartAt < idLength; substringStartAt += denominator)
+            {
+                var substring = idString.Substring(substringStartAt, denominator);
+
+                if (substring == firstSubstring)
+                {
+                    matches++;
+                    if (matches == maxDenominatorSubstringsInId)
+                        return true;
+                    continue;
+                }
+                else
+                    break;
+            }
+
+        }
+
+        return false;
     }
 }
