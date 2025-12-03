af2961feba65385c4b592bff6ccfb68c9112f79c
Stijn de Krieger <sdekrieger@auxilium.nl>
2025-12-03 11:36:01 +0100

    Finish day 3

--- AdventOfCode2025/Day03/Day03.cs
+++ AdventOfCode2025/Day03/Day03.cs
@@ -4,39 +4,52 @@ public class Day03
 {
     public static void Run()
     {
-        var banks = File.ReadAllLines("Day03/Data/Input.txt");
-        var totalJoltage = 0;
+        var batteryBanks = File.ReadAllLines("Day03/Data/Input.txt");
 
-        foreach (var bank in banks)
+        ulong totalJoltagePart1 = 0;
+        ulong totalJoltagePart2 = 0;
+
+        foreach (var bank in batteryBanks)
         {
-            var highestJoltage = FindHighestJoltageInBank(bank);
-            totalJoltage += highestJoltage;
+            totalJoltagePart1 += FindHighestJoltage(bank, 2);
+            totalJoltagePart2 += FindHighestJoltage(bank, 12);
         }
 
-        Console.WriteLine("Day 3 Part 1: " + totalJoltage);
-        Console.WriteLine("Day 3 Part 2: " + 0);
+        Console.WriteLine("Day 3 Part 1: " + totalJoltagePart1); //17109
+        Console.WriteLine("Day 3 Part 2: " + totalJoltagePart2); //169347417057382
     }
 
-    private static int FindHighestJoltageInBank(string stringBank)
+    private static ulong FindHighestJoltage(string batteryBank, int amountOfDigits)
     {
-        var bank = stringBank.Select(n => n - '0').ToList();
+        var bank = batteryBank.Select(n => n - '0').ToList();
+        var joltageString = "";
 
+        for (int i = amountOfDigits - 1; i >= 0; i--)
+        {
+            var highestValidNumber = FindNextHighestValidNumber(bank, i);
+            joltageString += highestValidNumber.ToString();
+
+            bank = bank.Skip(bank.IndexOf(highestValidNumber) + 1).ToList();
+        }
+
+        return ulong.Parse(joltageString);
+    }
+
+    private static int FindNextHighestValidNumber(List<int> bank, int digitsRemaining)
+    {
         int index = 0;
         while (true)
         {
             var highestNumber = bank.OrderByDescending(n => n).Skip(index).First();
             var bankRightOfHighest = bank.Skip(bank.IndexOf(highestNumber) + 1).ToList();
 
-            if (bankRightOfHighest.Count == 0)
+            if (bankRightOfHighest.Count < digitsRemaining)
             {
                 index++;
                 continue;
             }
             else
-            {
-                var nextHighest = bankRightOfHighest.Max();
-                return int.Parse(highestNumber.ToString() + nextHighest.ToString());
-            }
+                return highestNumber;
         }
     }
 }
