5a1f88cbd6b82105a5d4a4da78378c44c1c24fc9
Stijn de Krieger <sdekrieger@auxilium.nl>
2025-12-03 10:26:14 +0100

    Day 3 part 1

---
+++ AdventOfCode2025/Day03/Day03.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2025.Day03;
+
+public class Day03
+{
+    public static void Run()
+    {
+        var banks = File.ReadAllLines("Day03/Data/Input.txt");
+        var totalJoltage = 0;
+
+        foreach (var bank in banks)
+        {
+            var highestJoltage = FindHighestJoltageInBank(bank);
+            totalJoltage += highestJoltage;
+        }
+
+        Console.WriteLine("Day 3 Part 1: " + totalJoltage);
+        Console.WriteLine("Day 3 Part 2: " + 0);
+    }
+
+    private static int FindHighestJoltageInBank(string stringBank)
+    {
+        var bank = stringBank.Select(n => n - '0').ToList();
+
+        int index = 0;
+        while (true)
+        {
+            var highestNumber = bank.OrderByDescending(n => n).Skip(index).First();
+            var bankRightOfHighest = bank.Skip(bank.IndexOf(highestNumber) + 1).ToList();
+
+            if (bankRightOfHighest.Count == 0)
+            {
+                index++;
+                continue;
+            }
+            else
+            {
+                var nextHighest = bankRightOfHighest.Max();
+                return int.Parse(highestNumber.ToString() + nextHighest.ToString());
+            }
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCode2025.Day01;
 using AdventOfCode2025.Day02;
+using AdventOfCode2025.Day03;
 
 Console.WriteLine("Advent of Code 2025 by Stijn de Krieger");
 Day01.Run();
 Day02.Run();
+Day03.Run();
