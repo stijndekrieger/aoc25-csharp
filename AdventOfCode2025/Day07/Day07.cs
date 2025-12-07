e572b708b9004fb5b30828a739dd8e397ca708dc
stijndekrieger <stijndekrieger@gmail.com>
2025-12-07 22:49:30 +0100

    Day 7 part 1

--- AdventOfCode2025/Day07/Day07.cs
+++ AdventOfCode2025/Day07/Day07.cs
@@ -1,10 +1,62 @@
-﻿namespace AdventOfCode2025.Day07;
+﻿using AdventOfCode2025.Helpers;
+
+namespace AdventOfCode2025.Day07;
 
 public class Day07
 {
     public static void Run()
     {
-        Console.WriteLine("Day 7 Part 1: " + 0);
+        var diagram = GridHelper.GetGridFromTextInput(File.ReadAllLines("Day07/Data/input.txt"));
+
+        var startingColumn = 0;
+
+        for (int col = 0; col < diagram.GetLength(1); col++)
+        {
+            if (diagram[0, col] == 'S')
+            {
+                startingColumn = col;
+                break;
+            }
+        }
+
+        var amountOfSplits = 0;
+        var currentRow = 0;
+        var beamColumns = new List<int> { startingColumn };
+        var debugDiagram = (char[,])diagram.Clone();
+        while (true)
+        {
+            if (currentRow >= diagram.GetLength(0))
+                break;
+
+            var columnsToRemove = new List<int>();
+            var columnsToAdd = new List<int>();
+            foreach (var column in beamColumns)
+            {
+                if (diagram[currentRow, column] == '.')
+                {
+                    debugDiagram[currentRow, column] = '|';
+                    continue;
+                }
+                else if (diagram[currentRow, column] == '^')
+                {
+                    columnsToRemove.Add(column);
+                    columnsToAdd.AddRange(column + 1, column - 1);
+                    amountOfSplits++;
+                }
+            }
+
+            foreach (var col in columnsToRemove)
+                beamColumns.Remove(col);
+
+            beamColumns.AddRange(columnsToAdd);
+            beamColumns = beamColumns.Distinct().ToList();
+
+            //debugDiagram.PrintToConsole();
+            currentRow++;
+        }
+
+
+        Console.WriteLine("Day 7 Part 1: " + amountOfSplits);
         Console.WriteLine("Day 7 Part 2: " + 0);
     }
 }
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,5 +1,4 @@
 ﻿using AdventOfCode2025.Day01;
-using AdventOfCode2025.Day02;
 using AdventOfCode2025.Day03;
 using AdventOfCode2025.Day04;
 using AdventOfCode2025.Day05;
@@ -8,7 +7,7 @@ using AdventOfCode2025.Day07;
 
 Console.WriteLine("Advent of Code 2025 by Stijn de Krieger");
 Day01.Run();
-Day02.Run();
+//Day02.Run();
 Day03.Run();
 Day04.Run();
 Day05.Run();
