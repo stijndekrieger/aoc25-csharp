c3670f198d63f43c270e086da5eef2acba8b0425
stijndekrieger <stijndekrieger@gmail.com>
2025-12-07 22:14:23 +0100

    Day 7 setup

---
+++ AdventOfCode2025/Day07/Day07.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2025.Day07;
+
+public class Day07
+{
+    public static void Run()
+    {
+        Console.WriteLine("Day 7 Part 1: " + 0);
+        Console.WriteLine("Day 7 Part 2: " + 0);
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -4,6 +4,7 @@ using AdventOfCode2025.Day03;
 using AdventOfCode2025.Day04;
 using AdventOfCode2025.Day05;
 using AdventOfCode2025.Day06;
+using AdventOfCode2025.Day07;
 
 Console.WriteLine("Advent of Code 2025 by Stijn de Krieger");
 Day01.Run();
@@ -12,3 +13,4 @@ Day03.Run();
 Day04.Run();
 Day05.Run();
 Day06.Run();
+Day07.Run();
