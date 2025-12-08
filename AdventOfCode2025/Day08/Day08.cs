37ba7e6c563ada2ca65244d61747f0a97ee3eb87
stijndekrieger <stijndekrieger@gmail.com>
2025-12-08 19:33:15 +0100

    Day 8 progress

--- AdventOfCode2025/Day08/Day08.cs
+++ AdventOfCode2025/Day08/Day08.cs
@@ -2,15 +2,17 @@
 
 public class Day08
 {
-    private record CircuitBox(int X, int Y, int Z);
+    private record CircuitBox(int Id, int X, int Y, int Z);
 
     public static void Run()
     {
+        var circuitBoxId = 1;
         var circuitBoxes = File.ReadAllLines("Day08/Data/Input.txt")
             .Select(line =>
             {
                 var parts = line.Split(',');
                 return new CircuitBox(
+                    circuitBoxId++,
                     int.Parse(parts[0]),
                     int.Parse(parts[1]),
                     int.Parse(parts[2])
@@ -18,30 +20,60 @@ public class Day08
             })
             .ToList();
 
-        var shortestDistance = double.MaxValue;
-        CircuitBox shortestDistanceMainBox;
-        CircuitBox shortestDistanceSecondaryBox;
-        for (int i = 0; i < 1000; i++)
+        var circuits = new List<List<CircuitBox>>();
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("Iteration {0}", i);
-            foreach (var mainBox in circuitBoxes)
-            {
-                foreach (var secondaryBox in circuitBoxes)
-                {
-                    var distance = GetDistanceBetweenBoxes(mainBox, secondaryBox);
+            var (mainBox, secondaryBox) = GetClosestBoxes(circuitBoxes, circuits);
 
-                    if (distance != 0 && distance < shortestDistance)
-                    {
-                        shortestDistance = distance;
-                        shortestDistanceMainBox = mainBox;
-                        shortestDistanceSecondaryBox = secondaryBox;
-                    }
+            var existingCircuit = circuits.FirstOrDefault(c => c.Any(b => b.Id == mainBox.Id || b.Id == secondaryBox.Id));
+            if (existingCircuit is not null)
+            {
+                if (!existingCircuit.Any(b => b.Id == mainBox.Id))
+                    existingCircuit.Add(mainBox);
+                else if (!existingCircuit.Any(b => b.Id == secondaryBox.Id))
+                    existingCircuit.Add(secondaryBox);
+            }
+            else
+            {
+                var newCircuit = new List<CircuitBox> { mainBox, secondaryBox };
+                circuits.Add(newCircuit);
+            }
+        }
+
+        // Answer correct but not circuits
+        var part1Answer = circuits.OrderByDescending(c => c.Count).Take(3).Select(c => c.Count).Aggregate(1, (x, y) => x * y);
+
+        Console.WriteLine("Day 8 Part 1: {0}", part1Answer);
+        Console.WriteLine("Day 8 Part 2: {0}", 0);
+    }
+
+    private static (CircuitBox, CircuitBox) GetClosestBoxes(List<CircuitBox> circuitBoxes, List<List<CircuitBox>> circuits)
+    {
+        var shortestDistance = double.MaxValue;
+        CircuitBox? shortestDistanceMainBox = null;
+        CircuitBox? shortestDistanceSecondaryBox = null;
+        foreach (var mainBox in circuitBoxes)
+        {
+            foreach (var secondaryBox in circuitBoxes)
+            {
+                if (mainBox == secondaryBox)
+                    continue;
+
+                if (circuits.Any(c => c.Any(b => b.Id == mainBox.Id) && c.Any(b => b.Id == secondaryBox.Id)))
+                    continue;
+
+                var distance = GetDistanceBetweenBoxes(mainBox, secondaryBox);
+
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    shortestDistanceMainBox = mainBox;
+                    shortestDistanceSecondaryBox = secondaryBox;
                 }
             }
         }
 
-        Console.WriteLine("Day 8 Part 1: {0}", 0);
-        Console.WriteLine("Day 8 Part 2: {0}", 0);
+        return (shortestDistanceMainBox!, shortestDistanceSecondaryBox!);
     }
 
     private static double GetDistanceBetweenBoxes(CircuitBox mainBox, CircuitBox secondaryBox)
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,5 +1,4 @@
 ﻿using AdventOfCode2025.Day01;
-using AdventOfCode2025.Day02;
 using AdventOfCode2025.Day03;
 using AdventOfCode2025.Day04;
 using AdventOfCode2025.Day05;
@@ -9,7 +8,7 @@ using AdventOfCode2025.Day08;
 
 Console.WriteLine("Advent of Code 2025 by Stijn de Krieger");
 Day01.Run();
-Day02.Run();
+//Day02.Run();
 Day03.Run();
 Day04.Run();
 Day05.Run();
